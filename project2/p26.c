65b5b1a5c211f208c7a5a30b5820abe1f4b81035
Ramya <ramyaem4@gmail.com>
2018-09-18 14:55:44 +0530

    updated recurrsion program version-2

--- project2/p26.c
+++ project2/p26.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 long int rev(long int n)
 {
-    if(n==0)
-    return 0;
+    if(n<=9)
+    return 1;
     return 1+rev(n/10);
 }
 int main()
 {
-    printf("%ld",rev(234578642150832567856));
+    printf("%ld",rev(0));
 }
