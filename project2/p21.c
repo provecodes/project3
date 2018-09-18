f7da6b149730998312213ec5d3017dba89f2a9be
Ramya <ramyaem4@gmail.com>
2018-09-18 13:08:40 +0530

    1st program in project2

---
+++ project2/p21.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int p,q,sum;
+    printf("enter the values of a and b\n");
+    scanf("%d%d",&p,&q);
+    sum=0;
+    sum=p+q;
+    printf("sum=%d",sum);
+    return 0;
+}
